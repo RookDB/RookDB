76849d7d5a1554dedb51b6221514f4ec59490937
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2026-02-13 23:15:39 +0530

    Added Documentation Deployment Configurations

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,10 +14,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  url: "https://rookdb.github.io",
+  baseUrl: "/RookDB/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
