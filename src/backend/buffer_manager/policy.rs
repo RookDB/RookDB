6d23d7cc2d6b2b49ebc7606b9835aa2ca5c0c038
PrathamOP20 <pratham.pattanayak@students.iiit.ac.in>
2026-03-28 10:57:22 +0530

    Modified Code

--- src/backend/buffer_manager/policy.rs
+++ src/backend/buffer_manager/policy.rs
@@ -2,7 +2,7 @@ use super::frame::BufferFrame;
 
 pub trait ReplacementPolicy {
     // Select a victim frame for eviction
-    fn victim(&mut self, frames: &Vec<BufferFrame>) -> Option<usize>;
+    fn victim(&mut self, frames: &mut Vec<BufferFrame>) -> Option<usize>;
 
     // Called whenever a frame is accessed
     fn record_access(&mut self, frame_id: usize);
