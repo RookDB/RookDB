01aceda58f426a3a211fbc9d186dfb90b58e8093
hemanth-sunkireddy <hemanth-sunkireddy@users.noreply.github.com>
2026-04-26 09:17:06 +0000

    deploy: bd29010e1190636c153cdf875a73c79eb83211a8

--- rust-docs/trait.impl/core/default/trait.Default.js
+++ rust-docs/trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["storage_manager",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"storage_manager/backend/catalog/types/struct.Constraints.html\" title=\"struct storage_manager::backend::catalog::types::Constraints\">Constraints</a>",0]]]]);
+    const implementors = Object.fromEntries([["storage_manager",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"storage_manager/backend/catalog/types/struct.Constraints.html\" title=\"struct storage_manager::backend::catalog::types::Constraints\">Constraints</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- rust-docs/trait.impl/core/str/traits/trait.FromStr.js
+++ rust-docs/trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["storage_manager",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"storage_manager/backend/types/datatype/enum.DataType.html\" title=\"enum storage_manager::backend::types::datatype::DataType\">DataType</a>",0]]]]);
+    const implementors = Object.fromEntries([["storage_manager",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"storage_manager/backend/types/datatype/enum.DataType.html\" title=\"enum storage_manager::backend::types::datatype::DataType\">DataType</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
